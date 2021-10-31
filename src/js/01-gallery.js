03e343b98976c30340e46f1c23b9484488edcf40
Vitaliy <sivittat@gmail.com>
2021-10-31 11:08:20 +0200

    12

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+.map(item =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+      />
+      </a>
+      </div>`)
+      .join(``);
+      
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionDelay: 500,
+    showCounter: true,
+    enableKeyboard:	true,
+});
+
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {
+    id: 236203659,
+    width: 640
+});
+localStorage.setItem('videoplayer-current-time', time)
+
+player.on('timeupdate', function (data) {
+        // data is an object containing properties specific to that event
+    });
+
 
